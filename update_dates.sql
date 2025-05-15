9eea306e06ad287a17719b9749e46aa380f45f63
Usuario <usuario@exemplo.com>
2025-05-15 13:07:48 -0300

    Atualizado script SQL para corrigir problema de fuso horário nas datas

--- update_dates.sql
+++ update_dates.sql
@@ -1,20 +1,29 @@
--- Corrigir datas nos registros de leite
-UPDATE registros_leite
-SET data = date(date_trunc('day', data AT TIME ZONE 'UTC'));
+-- Script para corrigir as datas no banco de dados
+-- Este script adiciona 2 dias a todas as datas armazenadas para compensar o problema de fuso horário
 
--- Corrigir datas nos registros financeiros
-UPDATE registros_financeiros
-SET data = date(date_trunc('day', data AT TIME ZONE 'UTC'));
-
--- Corrigir datas nos alertas
-UPDATE alertas
-SET data = date(date_trunc('day', data AT TIME ZONE 'UTC'));
-
--- Corrigir datas nos animais
+-- Corrigir datas na tabela animais
 UPDATE animais
-SET data_nascimento = date(date_trunc('day', data_nascimento AT TIME ZONE 'UTC')),
-    data_proximo_parto = CASE 
-                          WHEN data_proximo_parto IS NOT NULL 
-                          THEN date(date_trunc('day', data_proximo_parto AT TIME ZONE 'UTC'))
-                          ELSE NULL 
-                        END; 
+SET data_nascimento = data_nascimento::date + interval '2 days'
+WHERE data_nascimento IS NOT NULL;
+
+UPDATE animais
+SET data_proximo_parto = data_proximo_parto::date + interval '2 days'
+WHERE data_proximo_parto IS NOT NULL;
+
+-- Corrigir datas na tabela alertas
+UPDATE alertas
+SET data = data::date + interval '2 days'
+WHERE data IS NOT NULL;
+
+-- Corrigir datas na tabela registros_leite
+UPDATE registros_leite
+SET data = data::date + interval '2 days'
+WHERE data IS NOT NULL;
+
+-- Corrigir datas na tabela registros_financeiros
+UPDATE registros_financeiros
+SET data = data::date + interval '2 days'
+WHERE data IS NOT NULL;
+
+-- Nota: Após executar este script, é importante continuar usando a função fixDateDisplay
+-- nos componentes do frontend para garantir que as datas sejam exibidas corretamente. 
